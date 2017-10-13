0322280b7d4888432d8a45d97e608f5564110b28
snellingm <snellingm@gilmour.org>
2017-10-13 14:45:43 -0400

    bug fix on quit_now

--- student.py
+++ student.py
@@ -113,20 +113,24 @@ class Piggy(pigo.Pigo):
             self.encR(10)
             self.encF(5)
 
-
-        if self.is_clear():
-            self.cruise()
-        else:
-            self.encR(10)
+    def nav(self):
+        """auto pilots and attempts to maintain original heading"""
+        logging.debug("Starting the nav method")
+        print("---------! NAVIGATION ACTIVATED !----------\n")
+        print("------ [ Press CTRL + C to stop me ] ------\n")
+        print("---------! NAVIGATION ACTIVATED !----------\n")
+        while True:
+            if self.is_clear():
+                self.cruise()
+            else:
+                self.encR(10)
 
     def cruise (self):
         """drive straight while path is clear"""
         self.fwd()
         while self.dist() > self.SAFE_STOP_DIST:
             time.sleep(.5)
-            self.stop()
-
-
+        self.stop()
 ####################################################
 ############### STATIC FUNCTIONS
 
