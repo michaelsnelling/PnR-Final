7b0b1f0f5bb244f900f992905a10d3ee5d0fe97f
snellingm <snellingm@gilmour.org>
2017-10-18 14:31:00 -0400

    bug fix on quit_now

--- student.py
+++ student.py
@@ -61,6 +61,7 @@ class Piggy(pigo.Pigo):
         """scans and estimates the number of obstacles within sight"""
         self.wide_scan()
         found_something = False
+        counter = 0
         for distance in self.scan:
             if distance and distance < 200 and not found_something:
                 found_something = True
