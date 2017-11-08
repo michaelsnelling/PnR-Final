20dbf99d3116da5b242a12ecdadd470fb5316f48
snellingm <snellingm@gilmour.org>
2017-11-08 14:55:20 -0500

    Fixing speed

--- student.py
+++ student.py
@@ -19,8 +19,8 @@ class Piggy(pigo.Pigo):
         # Our servo turns the sensor. What angle of the servo( ) method sets it straight?
         self.MIDPOINT = 77
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
-        self.SAFE_STOP_DIST = 30
-        self.HARD_STOP_DIST = 15
+        self.SAFE_STOP_DIST = 45
+        self.HARD_STOP_DIST = 25
         # YOU DECIDE: What left motor power helps straighten your fwd()?
         self.LEFT_SPEED = 190
         # YOU DECIDE: What left motor power helps straighten your fwd()?
