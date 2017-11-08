fffe470548263867d2cf50572454c429f5bef164
snellingm <snellingm@gilmour.org>
2017-11-08 14:51:57 -0500

    Fixing speed

--- student.py
+++ student.py
@@ -22,9 +22,9 @@ class Piggy(pigo.Pigo):
         self.SAFE_STOP_DIST = 30
         self.HARD_STOP_DIST = 15
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.LEFT_SPEED = 130
+        self.LEFT_SPEED = 140
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.RIGHT_SPEED = 150
+        self.RIGHT_SPEED = 160
         # This one isn't capitalized because it changes during runtime, the others don't
         self.turn_track = 0
         # Our scan list! The index will be the degree and it will store distance
