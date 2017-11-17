be262d5be068a6532b9a2e5c5ad87f01a720aeae
snellingm <snellingm@gilmour.org>
2017-11-17 14:43:46 -0500

    Full Obstacle count

--- student.py
+++ student.py
@@ -61,7 +61,7 @@ class Piggy(pigo.Pigo):
         counter = 0
         for x in range(5):
             counter += self.obstacle_count()
-            self.encR(9)
+            self.encR(8)
         print("\n-------I see %d object(s) total------\n" % counter)
 
     def obstacle_count(self):
